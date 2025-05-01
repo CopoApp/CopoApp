1eabebeab359624f195159da2bcb00ee61ba9604
Jordit <jordidiaz2020@gmail.com>
2025-05-01 15:00:06 -0400

    Remove Guest Relationships

--- server/db/migrations/20250429212523_create_users_table.js
+++ server/db/migrations/20250429212523_create_users_table.js
@@ -18,8 +18,6 @@ exports.up = function (knex) {
     table.decimal("location_latitude", 10, 8).nullable(); // 10 total digits, 8 after decimal
     table.decimal("location_longitude", 11, 8).nullable(); // 11 total digits, 8 after decimal
     table.integer("saved_pets_count").nullable();
-    // User role (Guest, User, Admin)
-    table.string("role").notNullable();
     // Creates created_at and updated_at columns and adjust accordingly
     table.timestamps(true, true); // first true: default to now, second true: allows updates
   });
