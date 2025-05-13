1dea34b6e3d11432a64a5a93de2033ccbff156af
Mario <airbomberofaqua@gmail.com>
2025-05-13 12:00:01 -0400

    remodeled the navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,6 +2,13 @@ import { useContext } from "react";
 import CurrentUserContext from "../contexts/current-user-context";
 import { NavLink, useParams } from "react-router-dom";
 
+/* a heck load of imports for the images */
+import bookmarkIcon from "../pages/Assets/saved.png";
+import feedIcon from "../pages/Assets/feed.png";
+import profileIcon from "../pages/Assets/profile.png";
+import myReportsIcon from "../pages/Assets/made.png";
+import newIcon from "../pages/Assets/create.png";
+
 const Navbar = () => {
   // if (!userId) return <></>;
   const { currentUser } = useContext(CurrentUserContext);
@@ -11,32 +18,32 @@ const Navbar = () => {
         to="/feed"
         className={({ isActive }) => `nav-item${isActive ? " active" : ""}`}
       >
-        <span className="nav-icon">🏠</span>
-        <span className="nav-text">Feed</span>
+      <img src={feedIcon} alt="Feed" className="nav-icon" />
+      <span className="nav-text">Feed</span>
       </NavLink>
 
       <NavLink
         to="/reports-log"
         className={({ isActive }) => `nav-item${isActive ? " active" : ""}`}
       >
-        <span className="nav-icon">📋</span>
-        <span className="nav-text">My Reports</span>
+      <img src={myReportsIcon} alt="My Reports" className="nav-icon" />
+      <span className="nav-text">My Reports</span>
       </NavLink>
 
       <NavLink
         to="/report"
         className={({ isActive }) => `nav-item${isActive ? " active" : ""}`}
       >
-        <span className="nav-icon">➕</span>
-        <span className="nav-text">New</span>
+      <img src={newIcon} alt="New" className="nav-icon" />
+      <span className="nav-text">New</span>
       </NavLink>
 
       <NavLink
         to={currentUser ? `/users/${currentUser.id}` : ``}
         className={({ isActive }) => `nav-item${isActive ? " active" : ""}`}
       >
-        <span className="nav-icon">👤</span>
-        <span className="nav-text">Profile</span>
+      <img src={profileIcon} alt="Profile" className="nav-icon" />
+      <span className="nav-text">Profile</span>
       </NavLink>
     </nav>
   );
